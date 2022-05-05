2b412261e9cb8f01c0c89f90671b7d1044aeed9a
majidassikiou <99686276+tricacole@users.noreply.github.com>
2022-05-06 01:40:42 +0200

    1

--- index.php
+++ index.php
@@ -2,9 +2,26 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Hello</title>
+    <title>Chat</title>
+    <link rel="stylesheet" href="styles.css">
   </head>
-  <body>
+  <body onload = "table();">
+    <script type="text/javascript">
+      function table(){
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function(){
+          document.getElementById("table").innerHTML = this.responseText;
+        }
+        xhttp.open("GET", "system.php");
+        xhttp.send();
+      }
 
+      setInterval(function(){
+        table();
+      }, 1000);
+    </script>
+    <div id="table">
+
+    </div>
   </body>
 </html>
