e67249a5e26d6ac304523beb69b2d27e14296930
majidassikiou <99686276+tricacole@users.noreply.github.com>
2022-05-06 02:12:20 +0200

    1

--- index.php
+++ index.php
@@ -1,27 +1,41 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Chat</title>
-    <link rel="stylesheet" href="styles.css">
-  </head>
-  <body onload = "table();">
-    <script type="text/javascript">
-      function table(){
-        const xhttp = new XMLHttpRequest();
-        xhttp.onload = function(){
-          document.getElementById("table").innerHTML = this.responseText;
-        }
-        xhttp.open("GET", "system.php");
-        xhttp.send();
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <title>Chat-Local</title>
+  <link rel="stylesheet" href="styles.css">
+  <script src="myscripts.js"></script>
+</head>
+
+<body onload="table();">
+  <script type="text/javascript">
+    function go() {
+    }
+    function table() {
+      const xhttp = new XMLHttpRequest();
+      xhttp.onload = function() {
+        document.getElementById("table").innerHTML = this.responseText;
+
+        var objDiv = document.getElementById("table");
+        objDiv.scrollTop = objDiv.scrollHeight;
+        console.log(objDiv.scrollTop)
+        console.log(objDiv.scrollHeight)
       }
+      xhttp.open("GET", "system.php");
+      xhttp.send();
+    }
+    setInterval(function() {
+      table();
+    }, 1000);
+  </script>
+  <div id="table">
+  </div>
 
-      setInterval(function(){
-        table();
-      }, 1000);
-    </script>
-    <div id="table">
-
-    </div>
-  </body>
+  <form action="insert.php" method="post" autocomplete="off">
+    <br><br>
+    <label>Escribe:</label><input type="text" name="Mensaje"><br><br>
+    <button type="submit" name="EnviarMensaje">Submit</button>
+  </form>
+</body>
 </html>
